4f4b4b599862a9b089c176d4eb7edae5d6bde04b
SpaceNoob123 <markusgame31@gmail.com>
2023-08-19 11:05:12 +0300

    release

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -29,12 +29,114 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.textBoxProcessName = new System.Windows.Forms.TextBox();
+            this.listViewBlacklist = new System.Windows.Forms.ListView();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.timerMonitoring = new System.Windows.Forms.Timer(this.components);
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.buttonStartMonitoring = new System.Windows.Forms.Button();
+            this.buttonStopMonitoring = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(12, 12);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 0;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // textBoxProcessName
+            // 
+            this.textBoxProcessName.Location = new System.Drawing.Point(12, 41);
+            this.textBoxProcessName.Name = "textBoxProcessName";
+            this.textBoxProcessName.Size = new System.Drawing.Size(163, 20);
+            this.textBoxProcessName.TabIndex = 1;
+            // 
+            // listViewBlacklist
+            // 
+            this.listViewBlacklist.GridLines = true;
+            this.listViewBlacklist.HideSelection = false;
+            this.listViewBlacklist.Location = new System.Drawing.Point(204, 41);
+            this.listViewBlacklist.Name = "listViewBlacklist";
+            this.listViewBlacklist.Size = new System.Drawing.Size(584, 125);
+            this.listViewBlacklist.TabIndex = 2;
+            this.listViewBlacklist.UseCompatibleStateImageBehavior = false;
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(204, 12);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(75, 23);
+            this.buttonRemove.TabIndex = 3;
+            this.buttonRemove.Text = "Remove";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // timerMonitoring
+            // 
+            this.timerMonitoring.Tick += new System.EventHandler(this.timerMonitoring_Tick);
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(204, 172);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.Size = new System.Drawing.Size(584, 266);
+            this.textBoxLog.TabIndex = 4;
+            // 
+            // buttonStartMonitoring
+            // 
+            this.buttonStartMonitoring.Location = new System.Drawing.Point(39, 297);
+            this.buttonStartMonitoring.Name = "buttonStartMonitoring";
+            this.buttonStartMonitoring.Size = new System.Drawing.Size(85, 24);
+            this.buttonStartMonitoring.TabIndex = 5;
+            this.buttonStartMonitoring.Text = "Start Monitor";
+            this.buttonStartMonitoring.UseVisualStyleBackColor = true;
+            this.buttonStartMonitoring.Click += new System.EventHandler(this.buttonStartMonitoring_Click);
+            // 
+            // buttonStopMonitoring
+            // 
+            this.buttonStopMonitoring.Location = new System.Drawing.Point(39, 327);
+            this.buttonStopMonitoring.Name = "buttonStopMonitoring";
+            this.buttonStopMonitoring.Size = new System.Drawing.Size(85, 20);
+            this.buttonStopMonitoring.TabIndex = 6;
+            this.buttonStopMonitoring.Text = "StopMonitor";
+            this.buttonStopMonitoring.UseVisualStyleBackColor = true;
+            this.buttonStopMonitoring.Click += new System.EventHandler(this.buttonStopMonitoring_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStopMonitoring);
+            this.Controls.Add(this.buttonStartMonitoring);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.listViewBlacklist);
+            this.Controls.Add(this.textBoxProcessName);
+            this.Controls.Add(this.buttonAdd);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.TextBox textBoxProcessName;
+        private System.Windows.Forms.ListView listViewBlacklist;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Timer timerMonitoring;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.Button buttonStartMonitoring;
+        private System.Windows.Forms.Button buttonStopMonitoring;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -1,20 +1,131 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using Formatting = Newtonsoft.Json.Formatting;
 
 namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        private List<string> forbiddenProcesses = new List<string>();
+        private const string blacklistFilePath = "blacklist.json";
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadBlacklist();
+            UpdateListView();
+        }
+
+        private void LoadBlacklist()
+        {
+            try
+            {
+                if (File.Exists(blacklistFilePath))
+                {
+                    string json = File.ReadAllText(blacklistFilePath);
+                    forbiddenProcesses = JsonConvert.DeserializeObject<List<string>>(json);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при загрузке черного списка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void SaveBlacklist()
+        {
+            try
+            {
+                string json = JsonConvert.SerializeObject(forbiddenProcesses, Formatting.Indented);
+                File.WriteAllText(blacklistFilePath, json);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка при сохранении черного списка: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void UpdateListView()
+        {
+            listViewBlacklist.Items.Clear();
+            foreach (var processName in forbiddenProcesses)
+            {
+                listViewBlacklist.Items.Add(processName);
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            string processName = textBoxProcessName.Text.Trim();
+            if (!string.IsNullOrEmpty(processName))
+            {
+                if (!forbiddenProcesses.Contains(processName))
+                {
+                    forbiddenProcesses.Add(processName);
+                    SaveBlacklist();
+                    UpdateListView();
+                }
+            }
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            if (listViewBlacklist.SelectedItems.Count > 0)
+            {
+                string selectedProcess = listViewBlacklist.SelectedItems[0].Text;
+                forbiddenProcesses.Remove(selectedProcess);
+                SaveBlacklist();
+                UpdateListView();
+            }
+        }
+
+        private void buttonStartMonitoring_Click(object sender, EventArgs e)
+        {
+            timerMonitoring.Start();
+            buttonStartMonitoring.Enabled = false;
+            buttonStopMonitoring.Enabled = true;
+        }
+
+        private void buttonStopMonitoring_Click(object sender, EventArgs e)
+        {
+            timerMonitoring.Stop();
+            buttonStartMonitoring.Enabled = true;
+            buttonStopMonitoring.Enabled = false;
+        }
+
+        private void timerMonitoring_Tick(object sender, EventArgs e)
+        {
+            foreach (var processName in forbiddenProcesses)
+            {
+                var processes = Process.GetProcessesByName(processName);
+                foreach (var process in processes)
+                {
+                    try
+                    {
+                        process.Kill();
+                        textBoxLog.AppendText($"Закрыт запрещенный процесс: {process.ProcessName}\r\n");
+                    }
+                    catch (Exception ex)
+                    {
+                        textBoxLog.AppendText($"Ошибка при попытке закрыть процесс {process.ProcessName}: {ex.Message}\r\n");
+                    }
+                }
+            }
         }
     }
 }
